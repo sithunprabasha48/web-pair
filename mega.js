413ee5f2479bb2ffa347261f67a61e9986515d81
sithunprabasha48 <sithunprabasha48@gmail.com>
2026-04-15 00:16:52 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -4,8 +4,8 @@ import fs from "fs";
 
 // Mega authentication credentials
 const auth = {
-    email: "", // your mega account login email
-    password: "", // your mega account login password
+    email: "sithunprabasha48@gmail.com", // your mega account login email
+    password: "20060109@@Sithun", // your mega account login password
     userAgent:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
